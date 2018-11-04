2a85d8bee1f9497a3cbc69c9df8a3b6dee72fc60
Yusuf Fadairo <yusuf.kami@gmail.com>
2018-11-04 16:34:11 -0500

    learn to spell

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -4,7 +4,7 @@ import './index.css';
 
 const LogoContainer = () =>
   <div className="centered animate-comet tc p-100" tabIndex="0" alt="Spacemango Inc.">
-    <img className="w-100p gitter" src={logoSvg} alt="Logo for Spacemango Inc."/>
+    <img className="w-100p jitter" src={logoSvg} alt="Logo for Spacemango Inc."/>
   </div>
 ;
 
