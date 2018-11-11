c3e4bfa6bcfba2dc88220e7d927e699c68176f4c
Yusuf Fadairo <yusuf.kami@gmail.com>
2018-11-11 14:53:34 -0500

    include footer

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from '../Header'
+import Footer from '../Footer'
 import Container from '../Container';
 import './index.css';
 
 const App = () =>
-  <div className='flex vw-100 vh-100 space-bg'>
-    <Header className="centered"/>
-    <Container />
+  <div className="w-100">
+    <div className='flex vw-100 vh-100 space-bg'>
+      <Header className="centered" />
+      <Container />
+    </div>
+    <Footer />
   </div>
   ;
 export default App;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+const Header = () =>
+  <div class="bg-white w-100 pa2">
+    <div class="tc">
+      <span class="black tc"> © 2018 Spacemango Inc.</span>
+    </div>
+  </div>
+  ;
+
+export default Header
