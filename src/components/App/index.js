f609cbf328adcecc9e189ca7b4210b7a68050327
Yusuf Fadairo <yusuf.kami@gmail.com>
2020-03-28 00:44:05 -0400

    remove loadng page magic from other pages

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,13 +4,30 @@ import Footer from '../Footer'
 import Container from '../Container';
 import './index.css';
 
-const App = () =>
-  <div className="w-100">
-    <div className='flex vw-100 vh-100 space-bg'>
-      <Header className="centered" />
-      <Container />
-    </div>
-    <Footer />
-  </div>
-  ;
+const LOADING_ANIMATION_DELAY = 2000;
+
+class App extends React.Component {
+  render() {
+    return <div className="w-100">
+      <div className='flex vw-100 vh-100 space-bg'>
+        <Header className="centered" />
+        <Container />
+      </div>
+      <Footer />
+    </div>;
+  }
+
+  componentDidMount() {
+    console.debug("App :: componentDidMount()");
+    console.debug("Timeout started!");
+    setTimeout(() => {
+    // document.getElementById('root').style.cssText = "";
+    // Perform animation to lift loading background
+    // Perform animation to send loading logo away
+    // perform animation to set logo to header
+    console.debug("Timeout complete!");
+    }, LOADING_ANIMATION_DELAY);
+  }
+}
+
 export default App;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,13 +7,7 @@ const Header = () =>
     <div className="h-100p centered">
       <Logo />
     </div>
-    <div className="flex p0 ma0">
-      <h1 className="f-lato white f2-ns f3">Spacemango Inc.</h1>
-    </div>
-    <div className="w-100 v-mid tc white pa0 ma0 pt1 animate-appear centered">
-      <a href="mailto:hi@spacemango.io" className="link light-gray tc dim hover-white">hi@spacemango.io</a>
-    </div>
   </div>
-;
+  ;
 
 export default Header
